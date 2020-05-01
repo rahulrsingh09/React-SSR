097c6521b3d06a15e7562f9bb9d694a2828c4abf
Rahul <rahsinwb@amazon.com>
2020-05-01 15:39:27 +0530

    initial

---
+++ src/client/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+    return (<div> I am the Home Component </div>)
+}
+
+export default Home;
---
+++ webpack.server.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    target: "node",
+
+    entry: "./src/index.js",
+
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "build"),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/',
+                options: {
+                    presets :[
+                        'react',
+                        'stage-0',
+                        ['env', {
+                            targets: {browsers: ['last 2 versions']}
+                        }]
+                    ]
+                }
+            }
+        ]
+    }
+};
