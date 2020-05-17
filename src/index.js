0d311c96cb48c4901a8c1f8362194408d72abd78
Rahul <rahsinwb@amazon.com>
2020-05-17 11:03:14 +0530

    heroku deployment

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ app.get('*', (req, res) => {
     store.close();
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log('Listening on Port 3000');
 })
